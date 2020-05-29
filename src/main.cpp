e690e0fc32a09fbd007b1d6c8a672283d728fe77
Katerina <kosareva.katerina2016@ya.ru>
2020-05-29 21:25:42 +0700

    Update for new function new_test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
+#include "header.h"
 #include <ctime>
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+int count_verb = 1, score = 150;
+
+struct verb slova[100];
+
 int main()
 {
-    char button;
+    int i = 0, button;
+    int count_attempt = 1;
+    struct glag g1;
     srand(time(0));
     setlocale(LC_ALL, "Rus");
     while (1) {
+        i = rand() % 100 + 1;
+        read_verb("./src/Slovar.txt");
         puts(" 1 Начать тест");
         puts(" 2 Словарь");
         puts(" 3 Начать заново ");
@@ -17,12 +26,17 @@ int main()
         scanf("%d", &button);
         switch (button) {
         case 1:
+            test(slova[i], g1, count_attempt, i, 0);
             break;
         case 2:
+            dictionary();
             break;
         case 3:
+            new_test("./src/Slovar2.txt", "./src/Rezultat.txt");
             break;
         case 4:
+            printf("%d\n", score);
+            rezult(score);
             break;
         case 0:
             return 0;
