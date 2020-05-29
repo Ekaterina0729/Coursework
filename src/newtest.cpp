c5ce42f77ddfe75045d40a2f637c53efe2ae6286
Koshetaur <koshetaur@yandex.ru>
2020-05-30 04:02:55 +0700

    remove file open check

--- src/newtest.cpp
+++ src/newtest.cpp
@@ -9,18 +9,11 @@
 int new_test(const string fname_S2, const string fname_R)
 {
     char button;
-    fstream S(fname_S2, ios_base::out);
-    fstream S1(fname_R, ios_base::out);
-    if (S.is_open() && S1.is_open())
-    {
-        printf("Программа обновлена! \n ");
-        S.close();
-        S1.close();
-    }
-    else{
-        printf("\n Error open file \n");
-        return 1;
-    }
+    ofstream S(fname_S2, ios_base::trunc);
+    ofstream S1(fname_R, ios_base::trunc);
+    printf("Программа обновлена! \n ");
+    S.close();
+    S1.close();
     printf("\n Введите любой символ для выхода в меню ");
     scanf("%s", &button);
     return 0;
