66bba157fdc48ef5097b0b8c1989da4b44ac9980
Koshetaur <koshetaur@yandex.ru>
2020-05-29 16:50:51 +0700

    create header.h

---
+++ src/header.h
@@ -0,0 +1,26 @@
+#ifndef CURS_H
+#define CURS_H
+
+#include <ctime>
+#include <iostream>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+struct verb {
+    char G1[100];
+    char G2[100];
+    char G3[100];
+    char G4[100];
+};
+struct glag {
+    char G1[100];
+    char G2[100];
+    char G3[100];
+};
+int read_verb(const string name);
+
+#endif
