a99ffd3f735156d0813d66a80447332df33aad3c
Koshetaur <koshetaur@yandex.ru>
2020-05-29 16:57:50 +0700

    write basic for function read_verb

---
+++ src/slovar.cpp
@@ -0,0 +1,15 @@
+#include "header.h"
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+extern struct verb v1;
+extern struct verb slova[];
+
+using namespace std;
+int read_verb(const string name)
+{
+    int i=0;
+    ifstream S(name, ios_base::in);
+    return i;
+}
