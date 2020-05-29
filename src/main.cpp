4175b4ed2064289891fe224699b7fd516d18e84e
Katerina <kosareva.katerina2016@ya.ru>
2020-05-29 15:14:39 +0700

    Created menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
+#include <ctime>
+#include <locale.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+    char button;
+    srand(time(0));
+    setlocale(LC_ALL, "Rus");
+    while (1) {
+        puts(" 1 Начать тест");
+        puts(" 2 Словарь");
+        puts(" 3 Начать заново ");
+        puts(" 4 Результат ");
+        puts(" 0 Закончить");
+        scanf("%d", &button);
+        switch (button) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 0:
+            return 0;
+        default:
+            puts(" error");
+        }
+    }
     return 0;
 }
