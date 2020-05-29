28038f69639ff04be93f240ba2ddffb589df499b
Кульбезекова Кристина <agnut666@gmail.com>
2020-05-30 03:13:04 +0700

    Create test for function data

--- test/test.cpp
+++ test/test.cpp
@@ -2,3 +2,14 @@
 #include "testparty/catch.hpp"
 
 using namespace std;
+
+TEST_CASE("DATA")
+{
+    CHECK(data("want") == 1);
+    CHECK(data("вуца") == 0);
+    CHECK(data("FDWEFdwq") == 0);
+    CHECK(data("wedfwfjfkfwefjwejkfkjwefjkwefjkewfjkwefjkewfjkewfjkkjewjkewkj")
+          == 0);
+    CHECK(data("!@$#") == 0);
+    CHECK(data("32435") == 0);
+}
