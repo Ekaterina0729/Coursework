708e01e55f749751cf1d5500c016483ff3220899
Кульбезекова Кристина <agnut666@gmail.com>
2020-05-30 03:48:06 +0700

    Create test for function rezult

--- test/test.cpp
+++ test/test.cpp
@@ -13,3 +13,10 @@ TEST_CASE("DATA")
     CHECK(data("!@$#") == 0);
     CHECK(data("32435") == 0);
 }
+TEST_CASE("REZULT")
+{
+    struct verb slova[100];
+    CHECK(rezult(150, slova) == 0);
+    CHECK(rezult(-10, slova) == 0);
+    CHECK(rezult(50, slova) == 0);
+}
